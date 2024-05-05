17407aaddaabfd17f651fb595f1e6902ac3a8bf4
Yesid Lopez <yesid.leonardo.lopez@hotmail.com>
2024-05-05 16:22:37 +0200

    Add MedicationType enum for different types of medications

---
+++ medivise/enums/medication_type.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+
+class MedicineType(Enum):
+    capsule = "Capsule"
+    tablet = "Tablet"
+    liquid = "Liquid"
+    topical = "Topical"
+    cream = "Cream"
+    device = "Device"
+    drops = "Drops"
+    foam = "Foam"
+    gel = "Gel"
+    soft_gel = "Soft Gels"
+    inhaler = "Inhaler"
+    injection = "Injection"
+    lotion = "Lotion"
+    ointment = "Ointment"
+    patch = "Patch"
+    powder = "Powder"
+    spray = "Spray"
+    suppository = "Suppository"
+    suspension = "Suspension"
